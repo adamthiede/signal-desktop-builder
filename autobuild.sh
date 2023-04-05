e90644accd19736227370fc28d8fd621a16bfa48
Adam Thiede <adamj@mailbox.org>
2023-04-05 07:11:45 -0500

    [no ci] update autobuild script

--- autobuild.sh
+++ autobuild.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 # An attempt at a cron job to auto build this thing
-# get latest signal version that isn't beta
 deps(){
 	dep_packages="jq curl"
 	sudo apt -qq install $dep_packages
 }
 
-#deps
 
 # get latest non-beta release version from github API
-latest_ver=$(curl -s https://api.github.com/repos/signalapp/signal-desktop/releases|jq -r .[].name|grep -v beta|head -n1)
+latest_ver=$(curl -s https://api.github.com/repos/signalapp/signal-desktop/releases|jq -r '.[] | select(.prerelease|not) | .name'|head -n1)
+
+# if run with "./autobuild.sh beta" then it will not filter out prerelease
+if [[ "$1" == "beta" ]];then
+	latest_ver=$(curl -s https://api.github.com/repos/signalapp/signal-desktop/releases|jq -r '.[] | .name'|head -n1)
+fi
+
 # make it an array, starting after the 'v'
 version="${latest_ver:1}"
-echo $version
-sleep 3
 readarray -d . -t vers <<< ${version}
 # branch is major.minor.x
 branch="${vers[0]}.${vers[1]}.x"
+
+echo "V $version Branch $branch"
+
+sleep 3
+
 ./update-node $branch
 # replace the clone line in Dockerfile with the new branch
 sed -e "s,RUN git clone https://github.com/signalapp/Signal-Desktop.*$,RUN git clone https://github.com/signalapp/Signal-Desktop -b $branch," -i Dockerfile
