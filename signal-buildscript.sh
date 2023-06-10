d12fd5f7e4c451eb75166b3cd95bef272682eedc
Adam Thiede <me@adamthiede.com>
2023-06-10 18:49:11 -0500

    comment some lines of the buildscript

--- signal-buildscript.sh
+++ signal-buildscript.sh
@@ -17,17 +17,17 @@ git revert 1ca0d821078286d5953cf0d598e6b97710f816ef
 # Dry run
 sed -r 's#("better-sqlite3": ").*"#\1file:../better-sqlite3"#' -i package.json
 # This may have to be cancelled and run again to get it to actually rebuild deps...
-yarn install # not recommended by signal, but required due to those two sed lines.
-yarn install --frozen-lockfile
-rm -rf ts/test-mock # also broken on arm64
-yarn generate
-yarn build:webpack
-#yarn test always fails on arm...
-yarn build:release --arm64 --linux --dir
-yarn build:release --arm64 --linux deb
-popd
-
-pushd /Signal-Desktop/release
-mv linux-arm64-unpacked signal
-tar cJvf signal-desktop_$(grep version ../package.json  | awk '{ print $2 }' | tr -d '",' | head -n 1).tar.xz signal
+# yarn install # not recommended by signal, but required due to those two sed lines.
+# yarn install --frozen-lockfile
+# rm -rf ts/test-mock # also broken on arm64
+# yarn generate
+# yarn build:webpack
+# #yarn test always fails on arm...
+# yarn build:release --arm64 --linux --dir
+# yarn build:release --arm64 --linux deb
+# popd
+# 
+# pushd /Signal-Desktop/release
+# mv linux-arm64-unpacked signal
+# tar cJvf signal-desktop_$(grep version ../package.json  | awk '{ print $2 }' | tr -d '",' | head -n 1).tar.xz signal
 
