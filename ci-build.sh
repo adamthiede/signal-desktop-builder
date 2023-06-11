16c605fa09048994ca50b362076deceff4684e83
Adam Thiede <me@adamthiede.com>
2023-06-11 12:29:47 -0500

    Revert "re-comment some things for github"
    This reverts commit 04d0811aa504a1fd3497af6fc16648b77ffbc9f2.

--- ci-build.sh
+++ ci-build.sh
@@ -7,15 +7,15 @@ podman create --name=signal-desktop-$VERSION -it localhost/signal-desktop-image-
 podman start signal-desktop-$VERSION
 ##podman exec -it --env-file=env -w /Signal-Desktop signal-desktop-$VERSION bash -c "echo $PATH"
 podman exec -it --env-file=env signal-desktop-$VERSION bash -i -c /signal-buildscript.sh
-#podman exec -it --env-file=env -w /Signal-Desktop signal-desktop-$VERSION yarn install --non-interactive
-#podman exec -it --env-file=env -w /Signal-Desktop signal-desktop-$VERSION yarn install --frozen-lockfile --non-interactive
-#podman exec -it --env-file=env -w /Signal-Desktop signal-desktop-$VERSION rm -rf ts/test-mock
-#podman exec -it --env-file=env -w /Signal-Desktop signal-desktop-$VERSION yarn generate
-#podman exec -it --env-file=env -w /Signal-Desktop signal-desktop-$VERSION yarn build:webpack
-#podman exec -it --env-file=env -w /Signal-Desktop signal-desktop-$VERSION yarn build:release --arm64 --linux --dir
-#podman exec -it --env-file=env -w /Signal-Desktop signal-desktop-$VERSION yarn build:release --arm64 --linux deb
+podman exec -it --env-file=env -w /Signal-Desktop signal-desktop-$VERSION yarn install --non-interactive
+podman exec -it --env-file=env -w /Signal-Desktop signal-desktop-$VERSION yarn install --frozen-lockfile --non-interactive
+podman exec -it --env-file=env -w /Signal-Desktop signal-desktop-$VERSION rm -rf ts/test-mock
+podman exec -it --env-file=env -w /Signal-Desktop signal-desktop-$VERSION yarn generate
+podman exec -it --env-file=env -w /Signal-Desktop signal-desktop-$VERSION yarn build:webpack
+podman exec -it --env-file=env -w /Signal-Desktop signal-desktop-$VERSION yarn build:release --arm64 --linux --dir
+podman exec -it --env-file=env -w /Signal-Desktop signal-desktop-$VERSION yarn build:release --arm64 --linux deb
 
-#podman exec -it --env-file=env -w /Signal-Desktop/release signal-desktop-$VERSION mv linux-arm64-unpacked signal
+podman exec -it --env-file=env -w /Signal-Desktop/release signal-desktop-$VERSION mv linux-arm64-unpacked signal
 podman exec -it --env-file=env -w /Signal-Desktop/release signal-desktop-$VERSION tar cJvf signal-desktop_${VERSION}.tar.xz signal
 
 podman cp signal-desktop-${VERSION}:/Signal-Desktop/release/signal-desktop_${VERSION}_arm64.deb ~/signal-desktop.deb
