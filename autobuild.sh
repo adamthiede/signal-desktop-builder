49d5585e50dbf5583a9150fdacd8e724586bca09
Adam Thiede <me@adamthiede.com>
2024-02-13 08:35:57 -0600

    autobuild.sh: better jqs

--- autobuild.sh
+++ autobuild.sh
@@ -7,11 +7,11 @@ deps(){
 
 
 # get latest non-beta release version from github API
-latest_ver=$(curl -s https://api.github.com/repos/signalapp/signal-desktop/releases|jq -r '.[] | select(.prerelease|not) | .tag_name'|head -n1)
+latest_ver=$(curl -s https://api.github.com/repos/signalapp/signal-desktop/releases|jq -r '[.[] | select(.prerelease|not).tag_name][0]')
 
 # if run with "./autobuild.sh beta" then it will not filter out prerelease
 if [[ "$1" == "beta" ]];then
-	latest_ver=$(curl -s https://api.github.com/repos/signalapp/signal-desktop/releases|jq -r '.[] | .tag_name'|head -n1)
+	latest_ver=$(curl -s https://api.github.com/repos/signalapp/signal-desktop/releases|jq -r '[.[].tag_name][0]')
 fi
 
 # determine if a build needs to be done at all
